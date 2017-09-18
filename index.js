29ebaa4e7e7d2f7870c3b87ac1507385ce3bddd0
jackson-lenhart <jacksonlenhart@gmail.com>
2017-09-18 12:57:04 -0400

    Back to array! :)

--- index.js
+++ index.js
@@ -7,17 +7,14 @@ const mappedPlayers = playersCSV.split("\n").map(item => item.split(","));
 const template = mappedPlayers[0];
 
 const reducedPlayers = mappedPlayers.slice(1).reduce((players, playerArr, index) => {
-  const newPlayer = template.reduce((player, attribute, attributeIndex) => {
-    return {
-      ...player,
-      [attribute]: playerArr[attributeIndex]
-    };
-  }, {});
-
-  return {
-    ...players,
-    [playerArr[0]]: newPlayer
-  };
-}, {});
+  return players.concat(
+    template.reduce((player, attribute, attributeIndex) => {
+      return {
+        ...player,
+        [attribute]: playerArr[attributeIndex]
+      };
+    }, {})
+  );
+}, []);
 
 fs.writeFileSync("./reduced-players.json", JSON.stringify(reducedPlayers, null, 2));
