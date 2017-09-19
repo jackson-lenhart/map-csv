da315472ebd8fc9904dc01720e3665869fff3567
jackson-lenhart <jacksonlenhart@gmail.com>
2017-09-19 18:55:04 -0400

    Back to one big object
    Using playerID’s as hashes

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const mappedPlayers = playersCSV.split("\n").map(item => item.split(","));
 
 const template = mappedPlayers[0];
 
-const reducedPlayers = mappedPlayers.slice(1).reduce((players, playerArr) => {
-  return players.concat(
-    template.reduce((player, attribute, attributeIndex) => {
-      player[attribute] = playerArr[attributeIndex];
-      return player;
-    }, {})
-  );
-}, []);
+const reducedPlayers = mappedPlayers.slice(1).reduce((players, playerArr, index) => {
+  players[playerArr[0]] = template.reduce((player, attribute, attributeIndex) => {
+    player[attribute] = playerArr[attributeIndex];
+    return player;
+  }, {});
+  return players;
+}, {});
 
 fs.writeFileSync("./reduced-players.json", JSON.stringify(reducedPlayers, null, 2));
