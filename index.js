193cf0089b5ec92170571a1c435830a2929bc2d5
jackson-lenhart <jacksonlenhart@gmail.com>
2017-09-25 13:45:06 -0400

    Nested for loops
    Achieve same function with nested for loops instead of nested calls to
    `reduce`.

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ const playersCSV = fs.readFileSync("./Master.csv", "utf-8");
 const mappedPlayers = playersCSV.split("\n").map(item => item.split(","));
 
 const template = mappedPlayers[0];
+const templateLen = template.length;
+const playersLen = mappedPlayers.length;
+let players = {};
+for (let i = 1; i < playersLen; i++) {
+  let player = {};
+  for (let j = 0; j < templateLen; j++) {
+    player[template[j]] = mappedPlayers[i][j];
+  }
+  players[mappedPlayers[i][0]] = player;
+}
 
-const reducedPlayers = mappedPlayers.slice(1).reduce((players, playerArr, index) => {
+/*const reducedPlayers = mappedPlayers.slice(1).reduce((players, playerArr) => {
   players[playerArr[0]] = template.reduce((player, attribute, attributeIndex) => {
     player[attribute] = playerArr[attributeIndex];
     return player;
   }, {});
   return players;
 }, {});
+*/
 
-fs.writeFileSync("./reduced-players.json", JSON.stringify(reducedPlayers, null, 2));
+fs.writeFileSync("./reduced-players.json", JSON.stringify(players, null, 2));
